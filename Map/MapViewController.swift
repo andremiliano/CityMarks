f91c0645fe7a1464b4b71c20f9eab75a1be30c89
Andre Emiliano <andre.novais.emiliano@gmail.com>
2023-12-02 00:15:53 +0000

    Updated map to show data

--- Map/MapViewController.swift
+++ Map/MapViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 import MapKit
 
-class MapViewController: UIViewController, CLLocationManagerDelegate {
+class MapViewController: UIViewController {
 
     var viewModel: MapViewModel?
-    private let locationManager = CLLocationManager()
+    private let mapView = MKMapView()
+    private var marks: [Mark]? {
+        didSet {
+            self.loadMap(with: marks)
+        }
+    }
 
     init(viewModel: MapViewModel) {
         self.viewModel = viewModel
@@ -24,25 +29,80 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        self.requestUserLocation()
+        self.viewModel?.requestUserLocation()
+        self.getMarksData()
     }
 
-    private func requestUserLocation() {
-        guard CLLocationManager.locationServicesEnabled() else {
-          return
+    private func getMarksData() {
+
+        self.viewModel?.onErrorHandling = { error in
+            let alert = UIAlertController(title: "There was an issue",
+                                          message: error.localizedDescription,
+                                          preferredStyle: .alert)
+
+            alert.addAction(UIAlertAction(title: "Ok",
+                                          style: .default))
+
+            self.present(alert, animated: true, completion: nil)
         }
 
-        self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
-
-        self.locationManager.delegate = self
-
-
-        switch locationManager.authorizationStatus {
-        case .notDetermined:
-            self.locationManager.requestWhenInUseAuthorization()
-        default:
-            self.locationManager.requestLocation()
+        self.viewModel?.onSuccess = { marks in
+            self.marks = marks
         }
+
+        self.viewModel?.getCitiesData()
+        self.setupMapView()
+    }
+
+    private func setupMapView() {
+        view.addSubview(self.mapView)
+        self.mapView.translatesAutoresizingMaskIntoConstraints = false
+        self.mapView.topAnchor.constraint(equalTo:view.topAnchor).isActive = true
+        self.mapView.leftAnchor.constraint(equalTo:view.leftAnchor).isActive = true
+        self.mapView.rightAnchor.constraint(equalTo:view.rightAnchor).isActive = true
+        self.mapView.bottomAnchor.constraint(equalTo:view.bottomAnchor).isActive = true
+
+//        self.addMapTrackingButton()
+    }
+
+    private func loadMap(with marks: [Mark]?) {
+        guard let marks,
+              let viewModel else {
+            return
+        }
+
+        var mapAnnotations: [MKPointAnnotation] = []
+        let annotation = MKPointAnnotation()
+        for mark in marks {
+            annotation.title = mark.name
+            annotation.coordinate = CLLocationCoordinate2D(latitude: mark.latitude, longitude: mark.longitude)
+            mapAnnotations.append(annotation)
+        }
+
+        let centerUser = viewModel.userLocation ??
+                        mapAnnotations.first?.coordinate ??
+                        CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
+
+        let region = MKCoordinateRegion(center: centerUser, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        self.mapView.showsUserLocation = viewModel.userLocation != nil
+        self.mapView.setRegion(region, animated: true)
+        self.mapView.addAnnotations(mapAnnotations)
+    }
+
+    private func addMapTrackingButton(){
+        let image = UIImage(systemName: "location.fill")?.withTintColor(.black)
+        let button   = UIButton(type: .custom) as UIButton
+        button.frame = CGRect(origin: CGPoint(x:10, y: 50), size: CGSize(width: 50, height: 50))
+        button.setImage(image, for: .normal)
+        button.backgroundColor = .clear
+        button.layer.cornerRadius = 5
+        button.layer.borderWidth = 1
+        button.layer.borderColor = UIColor.black.cgColor
+        button.addTarget(self, action: #selector(centerMapOnUserButtonClicked), for:.touchUpInside)
+        self.mapView.addSubview(button)
+    }
+
+    @objc func centerMapOnUserButtonClicked() {
+        self.mapView.setUserTrackingMode(.follow, animated: true)
     }
 }
--- Map/MapViewModel.swift
+++ Map/MapViewModel.swift
@@ -6,9 +6,14 @@
 //
 
 import Foundation
+import MapKit
 
 class MapViewModel: NSObject {
-    private var apiService : APIService!
+
+    private var apiService: APIService?
+    private let locationManager = CLLocationManager()
+
+    var userLocation: CLLocationCoordinate2D?
     var onSuccess : (([Mark]) -> Void)?
     var onErrorHandling : ((Error) -> Void)?
 
@@ -18,7 +23,11 @@ class MapViewModel: NSObject {
     }
 
     func getCitiesData() {
-        self.apiService.getCityMarks { [weak self] result in
+        guard let apiService else {
+            return
+        }
+
+        apiService.getCityMarks { [weak self] result in
             switch result {
             case .success(let cities):
                 let marks = cities.flatMap({ $0.marks })
@@ -32,4 +41,35 @@ class MapViewModel: NSObject {
             }
         }
     }
+}
+
+extension MapViewModel: CLLocationManagerDelegate {
+    func requestUserLocation() {
+        DispatchQueue.global().async {
+            guard CLLocationManager.locationServicesEnabled() else {
+                return
+            }
+
+            self.locationManager.desiredAccuracy = kCLLocationAccuracyHundredMeters
+
+            self.locationManager.delegate = self
+
+
+            switch self.locationManager.authorizationStatus {
+            case .notDetermined:
+                self.locationManager.requestWhenInUseAuthorization()
+            default:
+                self.locationManager.requestLocation()
+            }
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let userLocation: CLLocationCoordinate2D = manager.location?.coordinate else { return }
+        self.userLocation = userLocation
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+            print("Location manager failed with error: \(error.localizedDescription)")
+        }
 }
